a898c2ff773e0de3f4d028df1e096504bfd20230
calvin-li <calli@ucdavis.edu>
2015-10-04 00:16:03 -0700

    calculating averages

--- struct.c
+++ struct.c
@@ -8,8 +8,12 @@ struct student {
 };
 
 void main(void){
-   struct student sam = {85, 90, 88};
-   struct student tom = {93, 88, 91};
+   struct student sam = {85, 90, 88, 0};
+   sam.avg = (sam.mid+sam.final+sam.hws) / 3.0;
+   
+   struct student tom = {93, 88, 91, 0};
+   tom.avg = (tom.mid+tom.final+tom.hws) / 3.0;
+   
    struct student *he = &tom;  
    
    tom = sam;
